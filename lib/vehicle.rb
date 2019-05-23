6ce729757c3336b1b3920b8ee81a7016550bf4ef
Rachele Suy <rachelesuy@gmail.com>
2019-05-23 05:25:21 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -5,4 +5,13 @@ class Vehicle
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
+  
+   def go
+    "vrrrrrrrooom!"
+  end
+ 
+  def fill_up_tank
+    "filling up!"
+  end
+  
 end
